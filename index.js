a090f1e64f6dadabc8a588473793bb1cc5c32da8
Fantasy Shao <forever.fantasy27@gmail.com>
2014-07-30 23:28:37 +0800

    :high_brightness: generate directory success

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var fs   = require('fs'),
 var commander = require('commander'),
     mkdirp    = require('mkdirp');
 
+// Load Utilities
+var Util = require('./lib/util');
+var util = new Util();
+
 // Pacakge info
 var pkg = require('./package.json');
 
@@ -21,8 +25,17 @@ commander
 // Pcakge path
 var pkg_path = commander.args.shift() || '.';
 
+// Files
+var license = fs.readFileSync(__dirname + '/templates/LICENSE', 'utf-8');
+
 function generator() {
     console.log(pkg_path);
+
+    util.mkdir(pkg_path, function() {
+        util.mkdir(pkg_path + '/lib');
+        util.mkdir(pkg_path + '/bin');
+        util.writeFile(pkg_path + '/LICENSE', license);
+    });
 }
 
 module.exports = generator;
