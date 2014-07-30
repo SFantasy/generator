0b1f95edb57fe74d88ce88fb21c9ab6a947652a3
Fantasy Shao <forever.fantasy27@gmail.com>
2014-07-30 18:40:58 +0800

    Get path in the command. :smile:

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ var commander = require('commander'),
     mkdirp    = require('mkdirp');
 
 // Pacakge info
-var pkg = require('../package.json');
+var pkg = require('./package.json');
+
+commander
+    .version(pkg.version)
+    .on('--help', function() {
+        console.log('  Example:');
+        console.log('');
+        console.log('  $ npm-generator test');
+        console.log('');
+    }).parse(process.argv);
+
+// Pcakge path
+var pkg_path = commander.args.shift() || '.';
+
+function generator() {
+    console.log(pkg_path);
+}
+
+module.exports = generator;
